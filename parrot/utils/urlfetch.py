f451d8179dfc0652c5dcb47d1a4a8fe44fc536e5
unknown <longk10@MININT-2VHNT9Q.corp.emc.com>
2014-09-02 23:30:27 -0400

    add urlfetch

---
+++ parrot/utils/urlfetch.py
@@ -0,0 +1,15 @@
+from flask import g
+import requests
+import redis
+
+
+def fetch(url=None, *args, **kwargs):
+    pass
+
+
+def is_expired(*args, **kwargs):
+    pass
+
+
+def make_request(url=None):
+    pass
